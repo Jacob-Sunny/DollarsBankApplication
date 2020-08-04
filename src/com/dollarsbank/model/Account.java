fd065393c637e297305d94e4a5491bb7a6bcc64c
Jacob-Sunny <61558185+Jacob-Sunny@users.noreply.github.com>
2020-08-03 20:11:20 -0400

    Account class

--- src/com/dollarsbank/model/Account.java
+++ src/com/dollarsbank/model/Account.java
@@ -1,6 +1,63 @@
 package com.dollarsbank.model;
 
 public class Account {
+    //Decelerations
+    private String accountID;
+    private String initialDeposit;
+    private String CheckingAccount;
+    private String SavingsAccount;
+    private String logTime;
+    //parameterized constructor
+    public Account(String accountID, String initialDeposit, String checkingAccount, String savingsAccount, String logTime) {
+        this.accountID = accountID;
+        this.initialDeposit = initialDeposit;
+        CheckingAccount = checkingAccount;
+        SavingsAccount = savingsAccount;
+        this.logTime = logTime;
+    }
+    //default constructor
+    public Account() {
+    }
 
+    /************** GETTERS and SETTERS *********/
 
+    public String getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(String accountID) {
+        this.accountID = accountID;
+    }
+
+    public String getInitialDeposit() {
+        return initialDeposit;
+    }
+
+    public void setInitialDeposit(String initialDeposit) {
+        this.initialDeposit = initialDeposit;
+    }
+
+    public String getCheckingAccount() {
+        return CheckingAccount;
+    }
+
+    public void setCheckingAccount(String checkingAccount) {
+        CheckingAccount = checkingAccount;
+    }
+
+    public String getSavingsAccount() {
+        return SavingsAccount;
+    }
+
+    public void setSavingsAccount(String savingsAccount) {
+        SavingsAccount = savingsAccount;
+    }
+
+    public String getLogTime() {
+        return logTime;
+    }
+
+    public void setLogTime(String logTime) {
+        this.logTime = logTime;
+    }
 }
