a8721f1b65ac090591f9cf8b86c2f8bac845fd17
Jacob-Sunny <61558185+Jacob-Sunny@users.noreply.github.com>
2020-08-03 20:53:31 -0400

    ConsolePrinterUtility class and DollarsBankApplication class

--- src/com/dollarsbank/application/DollarsBankApplication.java
+++ src/com/dollarsbank/application/DollarsBankApplication.java
@@ -1,4 +1,46 @@
 package com.dollarsbank.application;
 
+import com.dollarsbank.utility.ConsolePrinterUtility;
+import com.dollarsbank.utility.ConsolePrinterUtility.Result;
+import com.dollarsbank.utility.ConsolePrinterUtility.TYPES;
+
 public class DollarsBankApplication {
-}
+
+    public static boolean exitFlag = true;
+
+    public static void main(String[] args) {
+
+    System.out.println("Welcome to Online Banking!!!");
+        do {
+            System.out.println("Please choose an option to proceed:");
+
+            System.out.println("1. Create a New Account");
+            System.out.println("2. Login");
+            System.out.println("3. Exit");
+
+            Result option = ConsolePrinterUtility.retryUntilSucceeds("Your input: ", TYPES.INT, 3);
+
+            switch (option.getValueAsInt()) {
+
+                case 1:
+                    System.out.println("1");
+                    break;
+
+                case 2:
+                    System.out.println("2");
+                    break;
+
+                case 3:
+                    exitFlag = false;
+                    System.out.println("Thanks for using online banking, have a great day!");
+            }
+
+        } while (exitFlag);
+
+        ConsolePrinterUtility.close();
+
+    }
+
+
+    }
+
